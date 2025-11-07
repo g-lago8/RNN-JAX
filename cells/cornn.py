19442060b8e54a1938c4da62169fb8d779464517
g-lago8 <lagomarsinigiacomo@gmail.com>
2025-11-07 15:15:56 +0100

    Add heterogeneous coRNN variant and delete unused imports

--- cells/cornn.py
+++ cells/cornn.py
@@ -2,10 +2,9 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import jax.random as jr
-import equinox as eqx
 from base import BaseCell
 from typing import Sequence, Callable, Tuple
-from jaxtyping import PyTree, Array, ArrayLike, Float, Inexact, Int, PRNGKeyArray, Complex
+from jaxtyping import Array, Float
 
 nonlinearity_dict={
     'relu': jax.nn.relu,
@@ -14,20 +13,27 @@ nonlinearity_dict={
 }
 
 class CoupledOscillatoryRNNCell(BaseCell):
-    gamma: float
-    eps: float
+    gamma: float | Array
+    eps: float | Array
     dt: float
     w_in: Float[Array, "hdim idim"]
     w_h: Float[Array, "hdim hdim"]
     w_z: Float[Array, "hdim hdim"] 
     b: Float[Array, "hdim"]
     nonlinearity:Callable
-    def __init__(self, idim, hdim, gamma, eps, dt, nonlinearity='relu', *, key):
+    def __init__(self, idim, hdim, gamma, eps, dt, nonlinearity='relu', heterogeneous=False, *, key):
         super().__init__(idim, hdim)
         self.states_shapes = ((hdim,), (hdim,))
         self.complex_state = False
-        self.gamma = gamma
-        self.eps = eps
+        if heterogeneous:
+            assert isinstance(gamma, Sequence) and len(gamma) == 2, "porcoddue bro"
+            assert isinstance(eps, Sequence) and len(eps) == 2, "ziopera fra"
+            gamma_key, eps_key, key = jr.split(key, 3)
+            self.gamma = jr.uniform(gamma_key, (hdim,), minval=gamma[0], maxval=gamma[1])
+            self.eps = jr.uniform(eps_key, (hdim,), minval=gamma[0], maxval=gamma[1])
+        else:
+            self.gamma = gamma
+            self.eps = eps
         self.dt = dt
         ikey, hkey, zkey = jr.split(key, 3)
         self.w_in = jr.uniform(ikey, (hdim, idim), minval= - 1 / jnp.sqrt(idim), maxval = 1 / jnp.sqrt(hdim))
@@ -40,8 +46,8 @@ class CoupledOscillatoryRNNCell(BaseCell):
     def __call__(self, x: Array, state: Tuple[Array, Array]) -> Tuple[Tuple[Array, Array], Array]:
         h, z = state
         z_new = z + self.dt * self.nonlinearity(self.w_h @ h + self.w_z @ z + self.w_in @ x + self.b ) \
-            - self.gamma * h - self.eps * z
+            - jax.lax.stop_gradient(self.gamma) * h - jax.lax.stop_gradient(self.eps) * z
         h_new = h + self.dt * z_new
-        return (h, z), h
+        return (h_new, z_new), h_new
     
 
