f1e30cff0da86bfc8766241dca095260115df9fb
g-lago8 <lagomarsinigiacomo@gmail.com>
2025-11-23 11:08:59 +0100

    Add custom LSTM cell and a notebook to benchmark it vs. EQX implementation

--- cells/__init__.py
+++ cells/__init__.py
@@ -1,6 +1,6 @@
 from cells.base import BaseCell
 from cells.antisymmetric import AntiSymmetricRNNCell, GatedAntiSymmetricRNNCell
-from cells.gated import LongShortTermMemory, GatedRecurrentUnit
+from cells.gated import LongShortTermMemory, GatedRecurrentUnit, LongShortTermMemoryCell
 from cells.obscillator import CoupledOscillatoryRNNCell
 from cells.unitary import UnitaryEvolutionRNNCell
 from cells.vanilla import ElmanRNNCell, IndRNNCell
--- cells/gated.py
+++ cells/gated.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Callable, Tuple
 import jax
 import jax.numpy as jnp
 import jax.random as jr
@@ -28,13 +28,84 @@ class LongShortTermMemory(BaseCell):
         self.complex_state = False
         self.states_shapes = ((hdim,), (hdim,))
         self.lstm = eqx.nn.LSTMCell(idim, hdim, key=key, **lstm_kwargs)
-        self.lstm = eqx.tree_at(lambda tree: tree.bias, self.lstm, jnp.ones_like(self.lstm.bias))
+        bias_modified = self.lstm.bias.at[2*hdim : 3*hdim].set(1.) # type: ignore
+        self.lstm = eqx.tree_at(lambda tree: tree.bias, self.lstm, bias_modified) # Initialize the forget gate bias to ones (biases are incorporated in one single vector)
 
     def __call__(
         self, x: jax.Array, state: Tuple[Array, ...]
     ) -> Tuple[Tuple[Array, Array], Array]:
         h, c = self.lstm(x, state)
         return (h, c), h
+
+
+class LongShortTermMemoryCell(BaseCell):
+    w_ih: Array # w input gate
+    w_ii: Array # w input gate
+    b_i : Array # b input gate
+    w_fh: Array # w forget gate
+    w_fi: Array # w forget gate
+    b_f: Array # b forget gate
+    w_oh: Array # w out gate
+    w_oi: Array # w out gate
+    b_o: Array # b forget gate
+    w_ci: Array # w for computing c
+    w_ch: Array # w for computing c
+    b_c : Array # b for computing c
+    nonlinearity: Callable
+
+    def __init__(self, idim, hdim, nonlinearity=jax.nn.relu, kernel_init=jax.nn.initializers.glorot_normal(), bias_init=jax.nn.initializers.zeros ,*, key, use_bias=None):
+        """LSTM cell
+
+        Args:
+            idim (int): input dimension
+            hdim (int): hidden dimension
+            key (PRNGKey): pseudo-RNG key
+            nonlinearity (Callable, optional): activation function (for gates, sigmoid is used). Defaults to jax.nn.relu.
+            kernel_init (jax.nn.Initializer, optional): weights initializer. Defaults to jax.nn.initializers.glorot_normal().
+            bias_init (jax.nn.Initializer, optional): bias initializer. Defaults to jax.nn.initializers.zeros, except for the *forget gate*, where the bias is always initialized to 1. 
+            To change the forget bias initialization (or to have finer-grained control on the initialized weights), one could use `equinox.tree_at` to perform model surgery as in the example below
+            ```
+            key = jax.random.key(42)
+            model_key, bias_key = jax.random.split(key, 2)
+            my_custom_bias = jax.nn.initializers.normal()(bias_key, hdim)
+            my_model = equinox.tree_at(
+                lambda model: model.b_f, # function that takes a pytree and return the leaf to modify
+                LongShortTermMemoryCell(idim, hdim, key=model_key), # the model to modify
+                my_custom_bias # the value to put in place of the chose leaf
+            )
+            ```
+            use_bias (bool, optional): not used for now (bias always on). Defaults to None.
+        """
+        self.idim = idim
+        self.hdim = hdim
+        self.complex_state = False
+        self.states_shapes = (hdim, hdim)
+        self.nonlinearity = nonlinearity
+        *subkeys, key = jr.split(key, 12)
+        self.w_ih = kernel_init(subkeys[0], (hdim, hdim))
+        self.w_fh = kernel_init(subkeys[1], (hdim, hdim))
+        self.w_oh = kernel_init(subkeys[2], (hdim, hdim))
+        self.w_ch = kernel_init(subkeys[3], (hdim, hdim))
+        self.w_ii = kernel_init(subkeys[4], (hdim, idim))
+        self.w_fi = kernel_init(subkeys[5], (hdim, idim))
+        self.w_oi= kernel_init(subkeys[6], (hdim, idim))
+        self.w_ci = kernel_init(subkeys[7], (hdim, idim))
+        self.b_i = bias_init(subkeys[8], (hdim,))
+        self.b_c = bias_init(subkeys[9], (hdim,))
+        self.b_o = bias_init(subkeys[10], (hdim,))
+        self.b_f = jnp.ones((hdim,))
+     
+
+    def __call__(self, x, state):
+        h, c = state
+        input_gate = jax.nn.sigmoid(self.w_ii @ x + self.w_ih @ h + self.b_i)
+        forget_gate = jax.nn.sigmoid(self.w_fi @ x + self.w_fh @ h + self.b_f)
+        output_gate = jax.nn.sigmoid(self.w_oi @ x + self.w_oh @ h + self.b_o)
+        c_int = self.nonlinearity(self.w_ci @ x + self.w_ch @ h + self.b_c)
+        c_new = jax.nn.sigmoid(input_gate * c_int + forget_gate * c)
+        h_new = self.nonlinearity(c_new) * output_gate
+        return (h_new, c_new), h_new
+
 
 
 class GatedRecurrentUnit(BaseCell):
