8c028ed6375814386fd2678d408ced04a079e6f1
g-lago8 <lagomarsinigiacomo@gmail.com>
2025-12-02 21:46:35 +0100

    Implement LRU layer

--- rnn_jax/ssm/base.py
+++ rnn_jax/ssm/base.py
@@ -1,43 +1,40 @@
-# This file implements the base class that should be subclassed when creating a new SSM. 
+# This file implements the base class that should be subclassed when creating a new SSM.
 # The structure of a forward pass of a linear SSM-lke model on a sequence xs is, using parallel scan
 # ========================================================================================
-# 1. Preprocess the input sequence xs in batches 
+# 1. Preprocess the input sequence xs in batches
 #    e.g. Bx_t = B @ x_t for each x_t in xs
-# 2. Apply the parallel scan on the binary associative function f(a, b) 
+# 2. Apply the parallel scan on the binary associative function f(a, b)
 # implementing the recursive pass, getting hidden states hs,
 #    e.g. h_t = A @ h_t-1 + Bx
-# 3. Postprocess the states in batches 
+# 3. Postprocess the states in batches
 #    e.g. y_t = C @ h_t + D @ x_t
 # ========================================================================================
 
 
-import jax
 from abc import ABC, abstractmethod
-from jax import random as jr
-from jax import nn 
-from jax import numpy as np
 import equinox as eqx
-from jax.lax import associative_scan
 from typing import TypeVar, Tuple
 from jaxtyping import Inexact, Array
 
-Ta = TypeVar('Ta')
-Tb = TypeVar('Tb')
+T = TypeVar("T")
+
 
 class BaseSSMLayer(eqx.Module, ABC):
-    """Base SSM module
-    """
+    """Base SSM module"""
+
+    in_dim: int
     state_dim: int
     model_dim: int
 
-    def __init__(self, state_dim: int, model_dim=None):
+    def __init__(self, in_dim: int, state_dim: int, model_dim=None):
+        self.in_dim = in_dim
         self.state_dim = state_dim
         if model_dim is None:
             model_dim = state_dim
         self.model_dim = model_dim
 
     @abstractmethod
-    def preprocess_inputs(self, xs:Inexact[Array, " seq_len model_dim"]):
+    def preprocess_inputs(self, xs: Inexact[Array, " seq_len model_dim"]):
         return xs
 
     @abstractmethod
@@ -45,7 +42,7 @@ class BaseSSMLayer(eqx.Module, ABC):
         pass
 
     @abstractmethod
-    def cell(self, a:Ta, b:Tb) -> Tuple[Ta, Tb]:
+    def ssm_cell(self, a: T, b: T) -> T:
         """A binary function given to the associative scan. Must be associative to work correctly
 
         Args:
@@ -53,11 +50,11 @@ class BaseSSMLayer(eqx.Module, ABC):
             b (Array): second input
         """
         pass
-    
+
     @abstractmethod
     def postprocess_outputs(self, xs, hs):
         pass
 
     @abstractmethod
     def __call__(self, xs, h0=None):
-       pass
+        pass
---
+++ rnn_jax/ssm/lru.py
@@ -0,0 +1,141 @@
+import jax
+from abc import ABC, abstractmethod
+from jax import random as jr
+from jax import nn
+import numpy as np
+from jax import numpy as jnp
+import equinox as eqx
+from jax.lax import associative_scan
+from typing import TypeVar, Tuple
+from jaxtyping import Inexact, Array
+from rnn_jax.ssm.base import BaseSSMLayer
+import einops
+
+
+class LinearRecurrentUnit(BaseSSMLayer):
+    nu_log: Inexact[Array, "state_dim"]
+    theta_log: Inexact[Array, "state_dim"]
+    W_in: Inexact[Array, "state_dim in_dim"]
+    W_out: Inexact[Array, "model_dim state_dim"]
+    W_skip: Inexact[Array, "model_dim in_dim"]
+    gamma_log: Inexact[Array, "state_dim"]
+
+    def __init__(
+        self,
+        in_dim,
+        state_dim: int,
+        model_dim=None,
+        rho_min=0.0,
+        rho_max=0.99,
+        theta_min=0,
+        theta_max=2 * np.pi,
+        *,
+        key,
+    ):
+        super().__init__(in_dim, state_dim, model_dim)
+        in_key, h_key, skip_key, out_key = jr.split(key, 4)
+        # init transition matrix parameters
+        self.nu_log, self.theta_log = self._init_W_hh(
+            rho_min, rho_max, theta_min, theta_max, h_key
+        )
+        # init input matrix
+        in_re_key, in_im_key = jr.split(in_key)
+        w_in_real = nn.initializers.glorot_normal()(
+            in_re_key, (self.state_dim, self.in_dim)
+        )
+        w_in_imag = nn.initializers.glorot_normal()(
+            in_im_key, (self.state_dim, self.in_dim)
+        )
+        self.W_in = w_in_real + 1j * w_in_imag
+        # init skip matrix
+        self.W_skip = nn.initializers.glorot_normal()(
+            skip_key, (self.model_dim, self.in_dim)
+        )
+        out_re_key, out_im_key = jr.split(out_key)
+        # init out matrix
+        w_out_real = nn.initializers.glorot_normal()(
+            out_re_key, (self.model_dim, self.state_dim)
+        )
+        w_out_imag = nn.initializers.glorot_normal()(
+            out_im_key, (self.model_dim, self.state_dim)
+        )
+        self.W_out = w_out_real + 1j * w_out_imag
+        # init gamma_log to
+        diag_lambda = jnp.exp(-jnp.exp(self.nu_log) + 1j * jnp.exp(self.theta_log))
+        self.gamma_log = jnp.log(jnp.sqrt(1 - jnp.abs(diag_lambda) ** 2))
+
+    def _init_W_hh(self, rho_min, rho_max, theta_min, theta_max, key):
+        key1, key2 = jr.split(key)
+
+        u1 = jr.uniform(key1, shape=(self.state_dim,))
+        u2 = jr.uniform(key2, shape=(self.state_dim,))
+
+        # sample the lenght of the eigenvalues
+        if rho_max < rho_min:
+            raise ValueError("rho_max must be larger than rho_min")
+        if rho_min < 0 or rho_max > 1:
+            raise ValueError("rho_min and rho_max must be in [0, 1]")
+        nu_log = np.log(-0.5 * np.log(u1 * (rho_max**2 - rho_min**2) + rho_min**2))
+        if theta_max < theta_min:
+            raise ValueError("theta_max must be larger than theta_min")
+
+        # sample the angles of the eigenvalues
+        # Make sure thetas are positive. One could want to sample negative thetas too,
+        # but the log breaks, so we just shift them by 2pi
+        while theta_min < 0:
+            theta_min += 2 * np.pi
+            theta_max += 2 * np.pi
+
+        theta_log = np.log(u2 * (theta_max - theta_min) + theta_min)
+
+        return nu_log, theta_log
+
+    def preprocess_inputs(self, xs):
+        scaled_W_in = einops.einsum(
+            self.W_in,
+            jnp.exp(self.gamma_log),
+            "state_dim in_dim, state_dim -> state_dim in_dim"
+        )
+        return jax.vmap(lambda x: scaled_W_in @ x)(xs)
+
+    def preprocess_matrix(self, seq_len):
+        lambda_matrix = jnp.exp(-jnp.exp(self.nu_log) + 1j * jnp.exp(self.theta_log))
+        return einops.repeat(
+            lambda_matrix, "state_dim -> seq_len state_dim", seq_len=seq_len
+        )
+
+    def ssm_cell(
+        self, a: Tuple[Array, Array], b: Tuple[Array, Array]
+    ) -> Tuple[Array, Array]:
+        matrix_pow_i, bx_i = a
+        matrix_pow_j, bx_j = b
+        return matrix_pow_i * matrix_pow_i, matrix_pow_j * bx_i + bx_j
+
+    def postprocess_outputs(self, xs, hs):
+        return (jax.vmap(lambda h: self.W_out @ h)(hs)).real + jax.vmap(lambda x: self.W_skip @ x)(xs)
+
+    def __call__(self, xs, h0=None):
+        seq_len = xs.shape[0]
+        lambda_elements = self.preprocess_matrix(seq_len)
+        w_in_xs = self.preprocess_inputs(xs)
+        scan_elements = (lambda_elements, w_in_xs)
+        lambda_powers, hs = associative_scan(self.ssm_cell, scan_elements)
+        return self.postprocess_outputs(xs, hs)
+
+
+def main():
+
+    xs = jr.uniform(jr.key(0), (1000, 1))
+    key = jr.key(1)
+    model = LinearRecurrentUnit(1, 16, 32, key=key)
+    jit_model = eqx.filter_jit(model)
+    ys = model(xs)
+    ys_prime = jit_model(xs)
+    assert jnp.allclose(ys, ys_prime, atol=1e-4)
+    print(ys[-1])
+    print(ys_prime[-1])
+    print("Check passed with rtol 1e-4")
+
+
+if __name__ == "__main__":
+    main()
