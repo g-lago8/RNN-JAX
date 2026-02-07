5e8e8dda545adf0f0e912061ea0cdee5e68bba8f
g-lago8 <lagomarsinigiacomo@gmail.com>
2026-02-07 11:11:57 +0100

    Add mlp and dummy mixer

--- rnn_jax/ssm/mixers.py
+++ rnn_jax/ssm/mixers.py
@@ -11,7 +11,15 @@ from rnn_jax.ssm.base import BaseSSMLayer
 import einops
 
 
-class GLUMixer(eqx.Module):
+class Mixer(eqx.Module):
+    def __init__(self, in_dim, out_dim, *, key):
+        pass
+
+    def __call__(self, x):
+        return x
+
+
+class GLUMixer(Mixer):
     gate_projection: eqx.nn.Linear
     main_projection: eqx.nn.Linear
     nonlinearity: Callable
@@ -31,3 +39,37 @@ class GLUMixer(eqx.Module):
 
     def __call__(self, x):
         return self.main_projection(x) * self.nonlinearity(self.gate_projection(x))
+
+
+class FFNMixer(Mixer):
+    pre_nonlinearity: Callable
+    post_nonlinearity: Callable
+    pre_projection: eqx.nn.Linear
+    post_projection: eqx.nn.Linear
+
+    def __init__(
+        self,
+        in_dim,
+        hidden_dim,
+        out_dim,
+        pre_nonlinearity=jax.nn.swish,
+        post_nonlinearity=jax.nn.swish,
+        *,
+        key
+        ):
+        key1, key2 = jr.split(key)
+        self.pre_projection = eqx.nn.Linear(in_dim, hidden_dim, key=key1)
+        self.post_projection = eqx.nn.Linear(hidden_dim, out_dim, key=key2)
+        self.pre_nonlinearity = pre_nonlinearity
+        self.post_nonlinearity = post_nonlinearity
+
+    def __call__(self, x):
+        h = self.pre_nonlinearity(self.pre_projection(x))
+        return self.post_nonlinearity(self.post_projection(h))
+    
+class IdentityMixer(Mixer):
+    def __init__(self, in_dim, out_dim, *, key):
+        pass
+
+    def __call__(self, x):
+        return x
