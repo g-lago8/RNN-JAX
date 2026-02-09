48fcdd34858ae81d57386b6e227b30c825e4e300
g-lago8 <lagomarsinigiacomo@gmail.com>
2026-02-09 20:21:00 +0100

    Import IdentityMixer, FFNMixer, and NonLinearIdentityMixer in ssm module

--- rnn_jax/ssm/__init__.py
+++ rnn_jax/ssm/__init__.py
@@ -1,5 +1,5 @@
 from rnn_jax.ssm.base import BaseSSMLayer
 from rnn_jax.ssm.lru import LinearRecurrentUnit
 from rnn_jax.ssm.s5 import SimplifiedStateSpaceLayer
-from rnn_jax.ssm.mixers import GLUMixer
+from rnn_jax.ssm.mixers import GLUMixer, IdentityMixer, FFNMixer, NonLinearIdentityMixer
 from rnn_jax.ssm.models import DeepStateSpaceModelEncoder
