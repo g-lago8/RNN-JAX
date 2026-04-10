4a62517bd02d62d89fad451a3531b93f5b139d7e
g-lago8 <lagomarsinigiacomo@gmail.com>
2026-04-10 10:50:56 +0200

    Update dependencies

--- rnn_jax/__init__.py
+++ rnn_jax/__init__.py
@@ -9,11 +9,11 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     # Inform type checkers / linters that these submodules exist without importing them at runtime
-    from . import cells, layers, utils  # noqa: F401
+    from . import cells, layers, utils, ssm, datasets  # noqa: F401
 # ...existing code...
-__version__ = "0.0.1"
+__version__ = "0.0.2"  # Define the version of the package
 
-__all__ = ["cells", "layers", "utils"]
+__all__ = ["cells", "layers", "utils", "ssm", "datasets"]  # List of submodules to be exposed when using `from rnn_jax import *`
 
 # Consumers can `import rnn_jax` and then import subpackages as needed:
 # from rnn_jax import cells
